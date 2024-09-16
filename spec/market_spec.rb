7512b0cadb811b17074bdee0c6b456589a8eb8c7
Paul Knapp <ppankluap@gmail.com>
2024-09-16 11:15:12 -0500

    test: add before each block for testing

---
+++ spec/market_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+RSpec.describe Item do 
+    before(:each) do
+        @market = Market.new("South Pearl Street Farmers Market")
+        @vendor1 = Vendor.new("Rocky Mountain Fresh")
+        @vendor3 = Vendor.new("Palisade Peach Shack")    
+        @vendor2 = Vendor.new("Ba-Nom-a-Nom") 
+        @item1 = Item.new({name: 'Peach', price: "$0.75"})
+        @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+        @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+        @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    end
+
+    

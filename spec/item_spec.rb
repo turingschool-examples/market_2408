2cf78fd91802d57db0efb4de13f2360c8b5dece4
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-16 09:51:33 -0600

    added last of the test to item

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -9,4 +9,17 @@ RSpec.describe Item do
         expect(@item).to be_a(Item)
     end
 
-    
+    it 'has a name' do
+        expect(@item.name).to eq("Peach")
+    end
+
+    it 'has a price' do
+        expect(@item.price).to eq(0.75)
+    end
+
+    it 'handles different prices' do
+        @item2 = Item.new({name: 'Tomato', price: "$0.50"})
+
+        expect(@item2.price).to eq(0.50)
+    end
+end

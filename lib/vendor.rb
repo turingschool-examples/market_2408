c63a53e37f96a03234f6d8c76673b865939bc9ea
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2024-09-16 10:28:13 -0500

    tests and code added for vendor info, some for items

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,3 +1,15 @@
 class Vendor
   
+    attr_reader :name, :inventory, :stock
+
+    def initialize(name)
+        @name = name
+        @inventory = {}
+    end
+
+    def check_stock(item)
+        @inventory
+    end
+
+
 end
--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -1,6 +1,6 @@
 require 'rspec'
 require './lib/item'
-require '.lib/vendor'
+require './lib/vendor'
 
 RSpec.describe Vendor do
     before(:each) do
@@ -11,3 +11,33 @@ RSpec.describe Vendor do
 
         expect(@vendor).to be_an_instance_of(Vendor)
     end
+
+    it 'can return the vendor name' do
+
+        expect(@vendor.name).to eq("Rocky Mountain Fresh")
+    end
+
+    it 'initially returns and empty hash of inventory' do
+        
+        expect(@vendor.inventory).to eq({})
+    end
+
+    it 'can check the stock of an item' do
+
+        expect(@vendor.check_stock(item1)).to eq(0)
+    end
+
+    it 'can stock and item' do
+        @vendor.stock(@item1, 30)
+
+        expect(@vendor.inventory).to include(@item1)
+        expect(@vendor.check_stock(@item1)).to eq(30)
+    end
+
+    it 'can stock more items' do
+        @vendor.stock(@item1, 25)
+
+        expect(@vendor.check_stock(@item1)).to eq(55)
+
+    end
+end

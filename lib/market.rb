02d14bf5bfa2c726c0a28d137bd141420c2ddd14
Alora Riley <alora.riley@gmail.com>
2024-09-16 10:16:09 -0600

    Add add vendor method

--- lib/market.rb
+++ lib/market.rb
@@ -1,7 +1,15 @@
+require_relative './item'
+require_relative './vendor'
+
 class Market
- attr_reader :name
+ attr_reader :name, :vendors
 
  def initialize(name)
   @name = name
+  @vendors = []
  end
+
+ def add_vendor(vendor)
+  @vendors << vendor
+ end 
 end

a7d88cbdb927602ff0fc88e6403002b5566981a7
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-16 10:01:10 -0600

    missed an end

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Vendor do
         @vendor = Vendor.new("Rocky Mountain Fresh")
         @item1 = Item.new({name: 'Peach', price: "$0.75"})
         @item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    end
     
     it 'exists' do
         expect(@vendor).to be_a(Vendor)

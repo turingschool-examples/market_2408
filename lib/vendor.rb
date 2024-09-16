a7c81435df04893f6108b4c9388e0b8e698d18cf
Seth Verrill <verrill.seth@gmail.com>
2024-09-16 09:32:38 -0600

    feat: Add initialize with class attributes

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,3 +1,21 @@
 class Vendor
+  attr_reader :name,
+              :inventory
+
+  def initialize(name)
+    @name = name
+    @inventory = {}
+  end
   
+  def check_stock(item)
+    @inventory.has_key?(item) ? @inventory[item] : 0
+  end
+
+  def stock(item, quantity)
+    if @inventory[item]
+      @inventory[item] += quantity
+    else
+      @inventory[item] = quantity
+    end
+  end
 end

d3ca6fc8b710c0903700d3171c0c4f442a43ccc7
Oscar Patterson <oepatterson12@gmail.com>
2024-09-16 12:33:07 -0400

    going

--- lib/item.rb
+++ lib/item.rb
@@ -3,8 +3,12 @@ attr_reader :name, :price
 
     def initialize (info)
         @name = info[:name]
-        @price = info[:price.gsub(/[^\d.]/, '').to_f]
+        @price = info[:price]
         #takes out all weird characters and symbols then convert to float
+    end
+
+    def price
+        @price.gsub(/[^\d.]/, '').to_f
     end
 
 end
--- lib/vendor.rb
+++ lib/vendor.rb
@@ -2,6 +2,7 @@ class Vendor
 
     def initialize(name)
         @name = name
+        @inventory = {}
     end
   
 end

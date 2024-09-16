5cfa3b2189def3b38dc2c489206bfd3cfeb4e4fd
Alora Riley <alora.riley@gmail.com>
2024-09-16 09:39:30 -0600

    Test can check stock and stock items

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 RSpec.describe Vendor do
   before(:each) do
     @vendor = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
   end
 
   it 'can initialize' do
@@ -12,4 +14,20 @@ RSpec.describe Vendor do
   it 'has a name' do
     expect(@vendor.name).to eq("Rocky Mountain Fresh")
   end
+
+  it 'starts with an empty inventory' do
+    expect(@vendor.inventory).to eq({})
+  end
+
+  it 'can check stock' do
+    expect(@vendor.check_stock(@item1)).to eq(0)
+  end
+
+  it 'can stock items' do
+    @vendor.stock(@item1, 30)
+
+    expect(@vendor.check_stock(@item1)).to eq(30)
+  end
+
+    
 end

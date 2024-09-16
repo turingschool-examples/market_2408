f7ed38da791fdc06d463ff6f8a8122226648dce3
Paul Knapp <ppankluap@gmail.com>
2024-09-16 11:12:30 -0500

    test: change name for easier testing

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 RSpec.describe Item do 
     before(:each) do
         @vendor1 = Vendor.new("Rocky Mountain Fresh")
-        @vendor2 = Vendor.new("Sysco Systems")
+        @vendor2 = Vendor.new("Palisade Peach Shack")
         @item1 = Item.new({name: 'Peach', price: "$0.75"})
         @item2 = Item.new({name: 'Tomato', price: '$0.50'})
     end
@@ -14,7 +14,7 @@ RSpec.describe Item do
 
     it '#has a name' do
         expect(@vendor1.name).to eq ("Rocky Mountain Fresh")
-        expect(@vendor2.name).to eq ("Sysco Systems")
+        expect(@vendor2.name).to eq ("Palisade Peach Shack")
     end
 
     it '#has inventory' do 

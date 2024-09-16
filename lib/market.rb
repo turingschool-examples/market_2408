4c29e1c48ce50ee640a450a71d7dd3f1336c81fd
Paul Knapp <ppankluap@gmail.com>
2024-09-16 11:48:31 -0500

    add vendor query methods to market class

--- lib/market.rb
+++ lib/market.rb
@@ -1,3 +1,16 @@
 class Market
- 
+    attr_reader :name, :vendors
+
+    def initialize(name)
+        @name = name
+        @vendors = ([])
+    end
+
+    def add_vendor(vendor)
+        @vendors << vendor
+    end
+
+    def vendor_names
+        @vendors.map { |vendor| vendor.name }
+    end
 end

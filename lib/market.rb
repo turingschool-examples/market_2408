83c5da09db739da3f4476c5d13a71c7057ade0c7
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-16 10:27:45 -0600

    added vendor methods to pass tests

--- lib/market.rb
+++ lib/market.rb
@@ -8,4 +8,12 @@ class Market
         @vendors = []
     end
  
+    def add_vendor(vendor)
+        @vendors << vendor
+    end
+
+    def vendor_names
+        @vendors.map(&:name)
+    end
+
 end

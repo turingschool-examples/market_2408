e67318dc698daa79a8d55e173f08cdc51b303619
Paul Knapp <ppankluap@gmail.com>
2024-09-16 10:35:23 -0500

    test: add before block and initialize test

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 RSpec.describe Item do 
     before(:each) do
-      
+        vendor = Vendor.new("Rocky Mountain Fresh")
     end
 
     it '#exists' do 
-        
+        expect(vendor).to be_an_instance_of(Vendor)
     end

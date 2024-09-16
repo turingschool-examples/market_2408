99771d6d0540af874e17d1eb688513ecd3060a1c
Paul Knapp <ppankluap@gmail.com>
2024-09-16 11:56:25 -0500

    feat:add vendors that sell method.

--- lib/market.rb
+++ lib/market.rb
@@ -13,4 +13,10 @@ class Market
     def vendor_names
         @vendors.map { |vendor| vendor.name }
     end
+
+    def vendors_that_sell(item)
+        @vendors.select do |vendor|
+            vendor.inventory.include?(item)
+        end
+    end
 end

bb2fd8b6bcb67636a4cbc72debd148f994847a79
Alora Riley <alora.riley@gmail.com>
2024-09-16 11:58:52 -0600

    Delete potential revenue test

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -29,6 +29,12 @@ RSpec.describe Market do
       @market.add_vendor(@vendor1)  
       @market.add_vendor(@vendor2)  
       @market.add_vendor(@vendor3)
+
+      @vendor1.stock(@item1, 35)   
+      @vendor1.stock(@item2, 7)
+      @vendor2.stock(@item4, 50)
+      @vendor2.stock(@item3, 25)
+      @vendor3.stock(@item1, 65)  
     end
     
     it 'can add vendors to the vendors array' do
@@ -40,11 +46,6 @@ RSpec.describe Market do
     end
 
     it 'can list vendors that sell' do
-      @vendor1.stock(@item1, 35)   
-      @vendor1.stock(@item2, 7)
-      @vendor2.stock(@item4, 50)
-      @vendor2.stock(@item3, 25)
-      @vendor3.stock(@item1, 65)  
 
       expect(@market.vendors_that_sell(@item1)).to match_array([@vendor1, @vendor3])
       expect(@market.vendors_that_sell(@item4)).to match_array([@vendor2])

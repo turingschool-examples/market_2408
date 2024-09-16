0b6eb849a0c3c0764755284ba3702ba0ac757c44
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2024-09-16 10:13:43 -0500

    added to name and price tests

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -15,11 +15,13 @@ RSpec.describe Item do
     it 'has a name' do
 
         expect(@item1.name).to eq("Peach")
+        expect(@item2.name).to eq("Tomato")
     end
 
     it 'has a price' do
 
         expect(@item1.price).to eq("0.75")
+        expect(@item2.price).to eq("0.50")
     end
 
 

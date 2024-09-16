23986108525492cc040d5d997968c9d49f237b81
Oscar Patterson <oepatterson12@gmail.com>
2024-09-16 12:57:05 -0400

    it1 doen

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,8 +1,28 @@
 class Vendor
+    attr_reader :name
+
+    attr_accessor :inventory
 
     def initialize(name)
         @name = name
         @inventory = {}
     end
   
+
+    def stock(item, amount)
+        if @inventory.include?(item)
+            @inventory[item] += amount
+        else
+        @inventory[item] = amount
+        end
+    end
+    
+    def check_stock(item)
+        if @inventory.include?(item) 
+            return  @inventory[item]
+        else
+            0
+        end
+    end
+
 end

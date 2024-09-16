f58758ded9fc10950cabaec97d377bd36d7f62a4
Jonathan Atkins <jonathanjatkins89@gmail.com>
2024-09-16 09:06:36 -0700

    Initial Commit

--- lib/market.rb
+++ lib/market.rb
@@ -1,3 +1,19 @@
+require 'pry'
 class Market
- 
+    attr_reader :name,
+                :vendors
+
+    def initialize(name)
+        @name    = name
+        @vendors = []
+    end
+
+    def add_vendor(vendor)
+        @vendors << vendor
+    end
+    
+    def vendor_names
+        
+    end
+    
 end
--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,3 +1,18 @@
+require 'pry'
 class Vendor
-  
+    attr_reader :name,
+                :inventory
+
+    def initialize(name)
+        @name      = name
+        @inventory = {}
+    end
+
+    def check_stock(item)
+        @inventory[item] || 0
+    end
+
+    def stock(item,quantity)
+        @inventory[item] = check_stock(item) + quantity
+    end
 end

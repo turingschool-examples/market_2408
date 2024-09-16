1172a50cb9356a41c1cc5cfed6f447e95b66c824
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2024-09-16 11:05:54 -0500

    created test and code to add vendors to market

--- lib/market.rb
+++ lib/market.rb
@@ -1,11 +1,15 @@
 class Market
 
     attr_reader :name, :vendors
+    attr_accessor :add_vendor
 
     def initialize(name)
         @name = name
         @vendors = []
+    end
 
+    def add_vendor(vendor)
+        @vendors << vendor
     end
  
 

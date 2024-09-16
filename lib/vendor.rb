c3a081409be79b223304bb2d59d617e21bf89614
Alora Riley <alora.riley@gmail.com>
2024-09-16 11:59:52 -0600

    Attempt to write method for potential revenue

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -17,6 +17,17 @@ class Vendor
     @inventory[item] = check_stock(item) + amount
   end
 
+  def revenue_item1
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    price = item1.price
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    amount = vendor1.inventory[item1]
+    price * amount
+  end
+
+  def potential_revenue
+    revenue_item1
+  end
 end
 
 

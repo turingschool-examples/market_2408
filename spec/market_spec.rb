4b6cfe2f290220df80eb4424141fd3b363e5cd45
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-16 13:59:54 -0400

    Starts I3

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -69,4 +69,18 @@ RSpec.describe Market do
         expect(@vendor2.potential_revenue).to eq(345.00)
         expect(@vendor3.potential_revenue).to eq(48.75)
     end
+
+    it 'can provide an overstocked items list' do
+        @market.add_vendor(@vendor1)
+        @market.add_vendor(@vendor2)
+        @market.add_vendor(@vendor3)
+        
+        @vendor1.stock(@item1, 35)
+        @vendor1.stock(@item2, 7)
+        @vendor2.stock(@item4, 50)
+        @vendor2.stock(@item3, 25)
+        @vendor3.stock(@item1, 65)
+        
+        @market.overstocked_items
+    end
 end

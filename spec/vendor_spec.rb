f0a175b1a5a5fec361cc74a793a5caef0d361bb1
Paul Knapp <ppankluap@gmail.com>
2024-09-16 10:37:50 -0500

    test: add all instantiations for testing

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -1,9 +1,13 @@
 require 'spec_helper'
 RSpec.describe Item do 
     before(:each) do
-        vendor = Vendor.new("Rocky Mountain Fresh")
+        @vendor1 = Vendor.new("Rocky Mountain Fresh")
+        @vendor2 = Vendor.new("Sysco Systems")
+        @item1 = Item.new({name: 'Peach', price: "$0.75"})
+        @item2 = Item.new({name: 'Tomato', price: '$0.50'})
     end
 
     it '#exists' do 
-        expect(vendor).to be_an_instance_of(Vendor)
+        expect(@vendor1).to be_an_instance_of(Vendor)
+        expect(@vendor2).to be_an_instance_of(Vendor)
     end

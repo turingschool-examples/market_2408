f7a2ae83cd8f8fbd6b324376653351909f5fe356
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-16 11:52:06 -0400

    Initializes, checks and adds stock - TESTS PASS

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,3 +1,17 @@
 class Vendor
-  
+    attr_reader :name,
+                :inventory
+
+    def initialize(name_param)
+        @name = name_param
+        @inventory = {}
+    end
+
+    def check_stock(item_param)
+        @inventory[item_param] || 0
+    end
+
+    def stock(item_param, quantity_param)
+        @inventory[item_param] = check_stock(item_param) + quantity_param
+    end
 end

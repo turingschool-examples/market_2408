3bddf0392d6a6dd860be63c1faa1d1c9c7ab955e
Eric DeShetler <ejdesh@gmail.com>
2024-09-16 09:10:28 -0700

    fix: Iteration 1 tests complete

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,3 +1,19 @@
 class Vendor
-  
+  attr_reader :name, :inventory
+
+  def initialize(name)
+    @name = name
+    @inventory = {}
+  end
+
+  def check_stock(item)
+    # 0 unless @inventory[item]
+    @inventory[item] || 0
+  end
+
+  def stock(item, quantity)
+    # binding.pry
+    @inventory[item] = quantity
+    @inventory[item]
+  end
 end

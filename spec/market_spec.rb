255fae5d5e2920107beab4e7e766bfa11233fda8
Alora Riley <alora.riley@gmail.com>
2024-09-16 10:27:29 -0600

    Test can list vendor name

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -24,11 +24,19 @@ RSpec.describe Market do
     expect(@market.vendors).to eq([])
   end
 
-  it 'can add vendors to the vendors array' do
-    @market.add_vendor(@vendor1)  
-    @market.add_vendor(@vendor2)  
-    @market.add_vendor(@vendor3)
+  describe '#Add vendors' do
+    before(:each) do
+      @market.add_vendor(@vendor1)  
+      @market.add_vendor(@vendor2)  
+      @market.add_vendor(@vendor3)
+    end
     
-    expect(@market.vendors).to eq([@vendor1, @vendor2, @vendor3])
+    it 'can add vendors to the vendors array' do
+      expect(@market.vendors).to eq([@vendor1, @vendor2, @vendor3])
+    end
+
+    it 'can list vendor name' do
+      expect(@market.vendor_names).to eq (["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+    end
   end
 end

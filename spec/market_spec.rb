144b0359598c2b49fb4c2547bf715fbe59f42de7
Alora Riley <alora.riley@gmail.com>
2024-09-16 10:16:35 -0600

    Test add vendor

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -3,6 +3,13 @@ require 'spec_helper'
 RSpec.describe Market do
   before(:each) do
     @market = Market.new("South Pearl Street Farmers Market") 
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor3 = Vendor.new("Palisade Peach Shack")    
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
   end
 
   it 'can be initialized' do
@@ -11,5 +18,17 @@ RSpec.describe Market do
 
   it 'has a name' do
     expect(@market.name).to eq("South Pearl Street Farmers Market")
+  end
+
+  it 'can list vendors' do
+    expect(@market.vendors).to eq([])
+  end
+
+  it 'can add vendors to the vendors array' do
+    @market.add_vendor(@vendor1)  
+    @market.add_vendor(@vendor2)  
+    @market.add_vendor(@vendor3)
+    
+    expect(@market.vendors).to eq([@vendor1, @vendor2, @vendor3])
   end
 end

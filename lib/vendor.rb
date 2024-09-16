3c4545de7177ad3f6be35c3522a45ec953de1349
Paul Knapp <ppankluap@gmail.com>
2024-09-16 11:02:22 -0500

    feat: add check stock method

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,3 +1,13 @@
 class Vendor
-  
+    attr_reader :name, :inventory
+    def initialize(name)
+        @inventory = {}
+        @name = name
+    end
+
+    def check_stock(item)
+        @inventory[item] || 0 
+    end
+
+    
 end

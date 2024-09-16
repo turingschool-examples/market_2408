309f40c8883e504796c63cdd6daeedb3b89a610f
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-16 12:07:48 -0400

    Creates file with initilize and add_Vendors methods

--- lib/market.rb
+++ lib/market.rb
@@ -1,3 +1,13 @@
 class Market
- 
+    attr_reader :name,
+                :vendors
+
+    def initialize(name_param)
+        @name = name_param
+        @vendors = []
+    end
+
+    def add_vendor(vendor_param)
+        @vendors << vendor_param
+    end
 end

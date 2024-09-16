8a43110b5c2443174c5670e18aebdf7d5d81d53c
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-16 10:00:34 -0600

    added stock methods and changed hash to .new because showed nil on tests

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -5,7 +5,14 @@ class Vendor
 
     def initialize(name)
         @name = name
-        @inventory = {}
+        @inventory = Hash.new(0)
     end
   
+    def check_stock(item)
+        @inventory[item]
+    end
+
+    def stock(item, quantity)
+        @inventory[item] += quantity
+    end
 end

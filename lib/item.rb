1c0b7926acf06105cc975927bc42d258f60eb7de
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-16 09:55:17 -0600

    Added attr to item class to make all tests pass

--- lib/item.rb
+++ lib/item.rb
@@ -1,3 +1,10 @@
 class Item
 
+    attr_reader :name,
+                :price
+
+    def initialize(details)
+        @name = details[:name]
+        @price = details[:price].delete('$').to_f
+    end
 end

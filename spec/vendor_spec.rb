7024bf993f0f5f159068f13c5c7dc273aa35d6e1
Alora Riley <alora.riley@gmail.com>
2024-09-16 09:24:04 -0600

    Fix tests to test vendor and not item

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -2,11 +2,14 @@ require 'spec_helper'
 
 RSpec.describe Vendor do
   before(:each) do
-    @item1 = Item.new({name: 'Peach', price: "$0.75"})
-    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @vendor = Vendor.new("Rocky Mountain Fresh")
   end
 
   it 'can initialize' do
-    expect(@item1).to be_instance_of(Vendor)
+    expect(@vendor).to be_instance_of(Vendor)
+  end
+
+  it 'has a name' do
+    expect(@vendor.name).to eq("Rocky Mountain Fresh")
   end
 end

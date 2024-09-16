965d3994692d9d08744c2067b277f4961ff9b6f9
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-16 10:20:42 -0600

    Finish making tests to follow interaction pattern

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -26,3 +26,20 @@ RSpec.describe Market do
         @market.add_vendor(@vendor3)
         expect(@market.vendors).to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
     end
+    
+    it 'can tell the vendors name' do
+        @market.add_vendor(@vendor1)
+        @market.add_vendor(@vendor2)
+        @market.add_vendor(@vendor3)
+        expect(@market.vendor_names).to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+    end
+
+    it 'can find vendors who sell a specific item' do
+        @market.add_vendor(@vendor1)
+        @market.add_vendor(@vendor2)
+        @market.add_vendor(@vendor3)
+        expect(@market.vendors_that_sell(@item1)).to eq([@vendor1, @vendor3])
+        expect(@market.vendors_that_sell(@item4)).to eq([@vendor2])
+    end
+end
+

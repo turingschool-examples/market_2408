c32039c479c48a43aaf6f6b8084b32dd291d6bd7
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-09-16 10:46:34 -0500

    feat created vendor class

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,3 +1,19 @@
 class Vendor
+  attr_reader :name,
+              :inventory
   
+  def initialize(name)
+    @name = name
+    @inventory = {}
+  end
+
+  def stock(item, amount)
+    @inventory[item] ||= 0
+    @inventory[item] += amount
+  end
+
+  def check_stock(item)
+    return 0 unless @inventory.include?(item)
+    @inventory[item]
+  end
 end

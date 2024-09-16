12cd29bba204ef313ac8e198f9dc3b34fdbfcf6f
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-16 11:32:54 -0600

    added more details to make total_inventory pass and also pass all prior tests

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -5,20 +5,20 @@ class Vendor
 
     def initialize(name)
         @name = name
-        @inventory = Hash.new(0)
+        @inventory = Hash.new { |hash, key| hash[key] = { quantity: 0 } }
     end
   
     def check_stock(item)
-        @inventory[item]
+        @inventory[item][:quantity]
     end
 
     def stock(item, quantity)
-        @inventory[item] += quantity
+        @inventory[item][:quantity] += quantity
     end
 
     def potential_revenue
-        @inventory.sum do |item, quantity|
-            item.price * quantity
+        @inventory.sum do |item, details|
+            item.price * details[:quantity]
         end
     end
 end

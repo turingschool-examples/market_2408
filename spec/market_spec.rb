14a8d973443e569287732c95ead4dd496ad3cd26
Paul Knapp <ppankluap@gmail.com>
2024-09-16 12:10:03 -0500

    test: add test for sorted items

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -49,4 +49,17 @@ RSpec.describe Market do
         expect(@market.vendors_that_sell(@item1)).to eq([@vendor1, @vendor3])
         expect(@market.vendors_that_sell(@item4)).to eq([@vendor2])
     end
+
+    it '#can show all items in inventory' do
+        @vendor1.stock(@item1, 35)    
+        @vendor1.stock(@item2, 7) 
+        @vendor2.stock(@item4, 50)    
+        @vendor2.stock(@item3, 25)
+        @vendor3.stock(@item1, 65)  
+        @market.add_vendor(@vendor1)    
+        @market.add_vendor(@vendor2)    
+        @market.add_vendor(@vendor3)
+        expect(@market.sorted_item_list).to eq("Banana Nice Cream", "Peach-Rasberry Nice Cream", "Peach", "Tomato")
+    end
 end
+

c6776799413360cd3fb3fcaa2fbb22bb42969f6e
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-09-16 10:11:52 -0500

    test: made tests for item

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,1 +1,30 @@
 require './spec_helper'
+
+RSpec.describe Item do
+  before(:each) do
+    @peach = Item.new({name: 'Peach', price: "$0.75"})
+    @tomato = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+
+  describe 'initialize' do
+    it 'can exist' do
+      expect(@peach).to be_an_instance_of(Item)
+      expect(@tomato).to be_an_instance_of(Item)
+    end
+
+    it 'has a name' do
+      expect(@peach.name).to eq("Peach")
+      expect(@tomato.name).to eq("Tomato")
+      expect(@tomato.name).to !eq("Tom")
+
+    end
+
+    it 'has a price' do
+      expect(@peach.price).to eq(.75)
+      expect(@tomato.price).to eq(.5)
+      expect(@peach.price).to !eq(.5)
+    end
+  end
+
+
+end
